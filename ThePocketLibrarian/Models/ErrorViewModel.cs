98fffb3763bc0227cb256f219f45ff571cc75cb8
Katelyn Yaeger <knyaeger93@gmail.com>
2023-08-11 17:57:52 -0500

    deleted file

---
+++ ThePocketLibrarian/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ThePocketLibrarian.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
